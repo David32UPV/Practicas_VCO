776943ad743a7c44531b82486666ca9bdac73775
David <dberlop@etsinf.upv.es>
2025-10-19 01:57:55 +0200

    Añadido lo del path a practica 2 y copiado carpeta images de la carpeta de pract1 a la carpeta de pract2

--- pract_2/ejercicio_1/1.py
+++ pract_2/ejercicio_1/1.py
@@ -4,8 +4,12 @@ import numpy as np
 import string 
 from PIL import Image
 import math 
+from pathlib import Path
 
-img = Image.open('Paisaje_de_Albacete.jpg')  
+# Obtener ruta relativa a la imagen
+image_path = Path(__file__).parent.parent / "images" / "Paisaje_de_Albacete.jpg"
+img = Image.open(image_path)
+
 img.size # (640, 480) 
 
 # Obtener la imagen transformada 
