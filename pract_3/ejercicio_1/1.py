90a1732899c4616e862643999b344488314dc786
Ricardo <rcasmar2@etsinf.upv.es>
2025-11-10 13:28:55 +0100

    cosas del 3

---
+++ pract_3/ejercicio_1/1.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from pathlib import Path 
+
+
+# Obtener ruta relativa a la imagen
+image_path = Path(__file__).parent.parent / "images" / "b1.jpg"
+img = np.asarray(Image.open(image_path))
+
+hr, edges_r = np.histogram(img[:,:,0],256)
+hg, edges_g = np.histogram(img[:,:,1],256)
+hb, edges_b = np.histogram(img[:,:,2],256)
+fig, axs = plt.subplots(1, 2, figsize=(12,4)) # Dos subplots en horizontal
+axs[1].stairs(hr, edges_r, label='Red histogram', ec='r')
+axs[1].stairs(hg, edges_g, label='Green histogram', ec='g')
+axs[1].stairs(hb, edges_b, label='Blue histogram', ec='b')
+axs[1].set_title("Step Histograms")
+axs[1].legend()
+plt.sca(axs[0]) # Para poner en el subplot izquierdo la imagen
+plt.imshow(img)
+plt.show()
